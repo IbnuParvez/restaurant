ef39432b5c3bf520cf83510ed02927fb915ff4b7
IbnuParvez <parvezfatma98@gmail.com>
2026-04-06 18:23:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import "./style.css";
 import homePage from "./home.js";
 import menuPage from "./menu.js";
 import contactPage from "./contact.js";
+
+const homeBtn = document.getElementById("home-button");
+const menuBtn = document.getElementById("menu-button");
+const contactBtn = document.getElementById("contact-button");
+const content = document.getElementById("content"); // Now 'content' can be used!
