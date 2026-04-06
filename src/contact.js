12aa59cd9c1f37449aab1828db2dba80bbed468b
IbnuParvez <parvezfatma98@gmail.com>
2026-04-06 17:55:27 +0300

    Add contact page and import stylesheet

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,20 @@
-function contactPage() {}
+function contactPage() {
+  const contactInfo = document.createElement("div");
+
+  const header = document.createElement("h2");
+  header.textContent = "Contact us";
+
+  const phone = document.createElement("p");
+  phone.textContent = "Phone: +1 234 567 890";
+
+  const address = document.createElement("p");
+  address.textContent = "Address: 777 Valhalla Gate";
+
+  const workingHours = document.createElement("p");
+  workingHours.textContent = "AsgardHours: Mon - Sun: 5pm - 11pm";
+
+  contactInfo.append(header, phone, address, workingHours);
+  return contactInfo;
+}
 
 export default contactPage;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import bgImage from "./bg.png";
+import "./style.css";
 import homePage from "./home.js";
 import menuPage from "./menu.js";
 import contactPage from "./contact.js";
